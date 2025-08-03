ab8b498568055c1b77552fabf27f6c337882b0f2
qzwang-gdm <qzwang@google.com>
2025-08-03 00:15:44 -0700

    Fix: Overhaul collision and movement logic for accuracy

--- script.js
+++ script.js
@@ -27,10 +27,8 @@ function collide(arena, player) {
     for (let y = 0; y < m.length; ++y) {
         for (let x = 0; x < m[y].length; ++x) {
             if (m[y][x] !== 0 &&
-                (o.x + x < 0 ||
-                 o.x + x >= arena[0].length ||
-                 o.y + y >= arena.length ||
-                 (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0)) {
+                (arena[y + o.y] &&
+                 arena[y + o.y][x + o.x]) !== 0) {
                 return true;
             }
         }
